c6923f7cbc84fe28be88cbce86dab215020ed140
Sherafgan <Kandov.Sh.S@sberbank.ru>
2019-04-04 12:02:05 +0300

    first commit: empty project

---
+++ src/main/java/ru/digitalsuperhero/dshapi/DshApiApplication.java
@@ -0,0 +1,13 @@
+package ru.digitalsuperhero.dshapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DshApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DshApiApplication.class, args);
+	}
+
+}
