de698084685b24c9a4b696e7007e146f1b82f775
Sherafgan <Kandov.Sh.S@sberbank.ru>
2019-04-04 18:40:04 +0300

    fixed login controller

--- src/main/java/ru/digitalsuperhero/dshapi/security/LoginController.java
+++ src/main/java/ru/digitalsuperhero/dshapi/security/LoginController.java
@@ -1,8 +1,12 @@
 package ru.digitalsuperhero.dshapi.security;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.digitalsuperhero.dshapi.dao.ContractorRepository;
 import ru.digitalsuperhero.dshapi.dao.CustomerRepository;
 import ru.digitalsuperhero.dshapi.dao.domain.Contractor;
@@ -24,22 +28,21 @@ public class LoginController {
     }
 
     @GetMapping(path = "/customer", consumes = "application/json", produces = "application/json")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public Customer login(@RequestBody Customer customer) {
+//    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<Customer> login(@RequestBody Customer customer) {
         Customer customerFound = customerRepo.findByEmail(customer.getUsername());
         if (customerFound != null && passwordEncoder.matches(customer.getPassword(), customerFound.getPassword())) {
-            return customerFound;
+            return new ResponseEntity<>(customer, HttpStatus.ACCEPTED);
         }
-        return customer;
+        return new ResponseEntity<>(customerFound, HttpStatus.NOT_FOUND);
     }
 
     @GetMapping(path = "/contractor", consumes = "application/json", produces = "application/json")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public Contractor login(@RequestBody Contractor contractor) {
+    public ResponseEntity<Contractor> login(@RequestBody Contractor contractor) {
         Contractor contractorFound = contractorRepo.findByEmail(contractor.getUsername());
         if (contractorFound != null && passwordEncoder.matches(contractor.getPassword(), contractorFound.getPassword())) {
-            return contractorFound;
+            return new ResponseEntity<>(contractor, HttpStatus.ACCEPTED);
         }
-        return contractor;
+        return new ResponseEntity<>(contractor, HttpStatus.NOT_FOUND);
     }
 }
