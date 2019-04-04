10b1cd341f825ef17ede915578b2d34d2c768223
Sherafgan <Kandov.Sh.S@sberbank.ru>
2019-04-04 13:36:26 +0300

    database config added

---
+++ src/main/java/ru/digitalsuperhero/dshapi/temp/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package ru.digitalsuperhero.dshapi.temp;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("$spring.datasource.url")
+    private String dbUrl;
+
+    @Bean
+    @Profile("prod")
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
