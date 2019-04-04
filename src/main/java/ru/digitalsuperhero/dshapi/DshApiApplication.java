6178fcc1e30ebf0f8f67eb5fec565c01c831917d
Sherafgan <Kandov.Sh.S@sberbank.ru>
2019-04-04 13:34:05 +0300

    remove security login page

--- src/main/java/ru/digitalsuperhero/dshapi/DshApiApplication.java
+++ src/main/java/ru/digitalsuperhero/dshapi/DshApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DshApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DshApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DshApiApplication.class, args);
+    }
 
 }
